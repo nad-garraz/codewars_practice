ed915abf7a654c74fe925921ac9e201c1b97a03d
Nad <nad.neutron@proton.me>
2022-07-10 23:11:51 +0200

    Polishing done

--- python/next_smaller/next_smaller.py
+++ python/next_smaller/next_smaller.py
@@ -16,33 +16,39 @@ test data only employs positive integers.
 The function you write for this challenge is the inverse of this kata: "Next bigger number with the same digits."
 """
 
+'''
+example:
+From a number n = 37124
+loop (backward) and find if next number is bigger --> 1<7
+get the max from the subList befor the 7 : [1,2,4] --> 4
+swap(7 and 4) --> [34127]
+sort the new subList, now before the 4 [1,2,7] --> [7,2,1]
+merge [3,4] + [1,2,7] --> [3,4,1,2,7]
+make int from list --> 34721  --> next smaller
+------
+special cases should be consider
+'''
 
 
-
-# Converts a list of one digit elements into an integer
-def list_of_digits_to_int(ls):
-    return int("".join([str(digit) for digit in ls]))
-
-
-# Works with the digits list reversed, i.e. the
-# comparison starts with the lower order numbers.
 def next_smaller(n):
     digits = [int(x) for x in str(n)]
-    if digits == list(sorted(digits)): return -1
+    if digits == list(sorted(digits)): return -1  # Trivial cases
 
     digits = list(reversed(digits))
     for idx in range(1, len(digits)):
         current = digits[idx]
-        prev = digits[idx-1]
-        if current > prev:
+
+        if current > digits[idx-1]:  # current digit must be bigger than digit to the left to swap
             tail = list(reversed(sorted(digits[:idx])))  # from start to prev. To get max
             idx_max = 0
+
             while current <= tail[idx_max]:  # looking for tail_max, but smaller than current
                 idx_max += 1
-            digits[idx], digits[idx_max] = digits[idx_max], digits[idx]
+
+            digits[idx], digits[idx_max] = digits[idx_max], digits[idx]  # swap
             answer = list(reversed(digits[idx:])) + list(reversed(sorted(digits[:idx])))
             if answer[0] == 0: return -1  # Number with leading zeroes are not valid
-            return list_of_digits_to_int(answer)
+            return int("".join([str(digit) for digit in answer]))  # make int from list
     return n
 
 
